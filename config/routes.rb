b9075fc79f66b26f111cad28a70ee41db23709e1
Dominic Padula <domopadula2192@gmail.com>
2021-02-03 18:19:28 -0700

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+  
   get '/exhibits', to: 'exhibits#index'
   get '/exhibits/new', to: 'exhibits#new'
   post '/exhibits', to: 'exhibits#create'
