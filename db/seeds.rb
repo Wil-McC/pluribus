04b069ca47ed30635991f39e56e6dcc6a4369b4e
Wil McCauley <wmccauley724@gmail.com>
2021-02-04 17:29:15 -0700

    Add seed code for museums

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Artifact.destroy_all
+Museum.destroy_all
+
+basel = Museum.create!(name: "ArtBasel", open: true, cost: 18)
+demet = Museum.create!(name: "Demetera", open: true, cost: 4)
+forte = Museum.create!(name: "ItaliForte", open: false, cost: 8)
+sommel = Museum.create!(name: "Sommelitsch", open: false, cost: 12)
+camber = Museum.create!(name: "CamberMass", open: true, cost: 6)
+chengdu = Museum.create!(name: "YiShu Chengdu", open: true, cost: 0)
+
+basel.artifacts.create(name: "Die Welt", rare: true, age: 230)
+basel.artifacts.create(name: "Die Welt", rare: true, age: 230)
