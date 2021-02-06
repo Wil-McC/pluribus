884e5e8ad1fb3ef2ddaeb62cad9ac591ebfeffa3
Wil McCauley <wmccauley724@gmail.com>
2021-02-06 16:01:48 -0700

    Add test for new museum link path

--- spec/features/museums/index_spec.rb
+++ spec/features/museums/index_spec.rb
@@ -10,12 +10,20 @@ RSpec.describe 'the museums index page' do
     expect(page).to have_content(basel.name)
     expect(page).to have_content(demet.name)
   end
+
   it 'has a button for museum creation' do
     basel = Museum.create!(name: "ArtBasel", open: true, cost: 18)
     demet = Museum.create!(name: "Demetera", open: true, cost: 4)
 
     visit '/museums'
-    
+
     expect(page).to have_link("New Museum")
+  end
+
+  it 'sends user to new museum form on clicking new museum' do
+    visit '/museums'
+
+    click_link "New Museum"
+    expect(current_path).to eq('/museums/new')
   end
 end
