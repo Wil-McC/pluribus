871a3844f56502f4fc90585aa3953eb35e047a2a
Wil McCauley <wmccauley724@gmail.com>
2021-02-09 16:25:51 -0700

    Add passing test for class method show_rare

--- spec/models/artifact_spec.rb
+++ spec/models/artifact_spec.rb
@@ -4,4 +4,17 @@ describe Artifact, type: :model do
   describe 'relationships' do
     it { should belong_to :museum}
   end
+
+  describe 'class methods' do
+    describe '::show_rare' do
+      it 'returns only rare items' do
+        sommel = Museum.create!(name: "Sommelitsch", open: false, cost: 12)
+        xtre = sommel.artifacts.create(name: "Xtreem", rare: false, age: 22)
+        phin = sommel.artifacts.create(name: "Phineastris", rare: true, age: 845)
+        anth = sommel.artifacts.create(name: "Anthobloc", rare: false, age: 54)
+
+        expect(Artifact.show_rare).to eq([phin])
+      end
+    end
+  end
 end
