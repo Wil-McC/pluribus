6a4d232f2caef79b7ca7021947b1e491a465a30d
Wil McCauley <wmccauley724@gmail.com>
2021-02-09 17:29:40 -0700

    Add tests for update and delete from stories 20 and 21

--- spec/features/artifacts/index_spec.rb
+++ spec/features/artifacts/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the artifact index page' do
-  it 'shows each artifact with attributes' do
+  it 'shows rare artifacts with attributes' do
     basel = Museum.create!(name: "ArtBasel", open: true, cost: 18)
     welt = basel.artifacts.create(name: "Die Welt", rare: true, age: 230)
     blau = basel.artifacts.create(name: "Unterwasser Blau", rare: false, age: 76)
@@ -15,4 +15,36 @@ RSpec.describe 'the artifact index page' do
     expect(page).to have_content(welt.created_at)
     expect(page).to have_content(welt.updated_at)
   end
+
+  it 'has an update link associated with each artifact' do
+    basel = Museum.create!(name: "ArtBasel", open: true, cost: 18)
+    welt = basel.artifacts.create(name: "Die Welt", rare: true, age: 230)
+    blau = basel.artifacts.create(name: "Unterwasser Blau", rare: false, age: 76)
+    amber = basel.artifacts.create(name: "Amberzauber Sieben", rare: false, age: 250)
+
+    visit '/artifacts'
+
+    expect(page).to have_link('Update Die Welt')
+
+    click_link 'Update Die Welt'
+
+    expect(current_path).to eq("/artifacts/#{welt.id}/edit")
+  end
+
+  it 'has a delete link associated with each artifact' do
+    basel = Museum.create!(name: "ArtBasel", open: true, cost: 18)
+    welt = basel.artifacts.create(name: "Die Welt", rare: true, age: 230)
+    blau = basel.artifacts.create(name: "Unterwasser Blau", rare: false, age: 76)
+    amber = basel.artifacts.create(name: "Amberzauber Sieben", rare: false, age: 250)
+
+    visit '/artifacts'
+
+    expect(page).to have_link('Delete Die Welt')
+
+    click_link 'Delete Die Welt'
+
+    expect(current_path).to eq("/artifacts")
+    expect(page).to_not have_content(welt.name)
+  end
+
 end
