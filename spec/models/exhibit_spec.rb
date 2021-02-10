d83eb35a8ef65da911e35e405df17b268fec7079
Dominic Padula <domopadula2192@gmail.com>
2021-02-10 08:17:42 -0700

    add test for searching by full name

--- spec/models/exhibit_spec.rb
+++ spec/models/exhibit_spec.rb
@@ -30,4 +30,34 @@ describe Exhibit, type: :model do
   expected = black_holes.sort_alpha
   expect(expected).to eq([alyssa, dominic, george])
   end
+
+  #story 25
+  it "It is able to sort by number of patrons attending" do
+    Exhibit.destroy_all
+    black_holes = Exhibit.create! name:"Black Holes", open:true, cost:75
+    king_tut = Exhibit.create! name:"King Tut", open:true, cost:40
+    before_us= Exhibit.create! name:"Before Us", open:true, cost:35
+
+    dominic = king_tut.patrons.create!(name: "Dominic", paid:true, age:28)
+    alyssa = king_tut.patrons.create!(name: "Alyssa", paid:false, age:25)
+
+    george = before_us.patrons.create!(name: "George", paid:false, age:19)
+    johnny = before_us.patrons.create!(name: "Johnny", paid:false, age:19)
+
+    ron = black_holes.patrons.create!(name: "Ron", paid:false, age:19)
+    expected = Exhibit.all.sort_by_children
+    expect(expected).to eq([king_tut, before_us, black_holes])
+
+  end
+
+  # story 26
+  it "is able to search by complete names" do
+    black_holes = Exhibit.create! name:"Black Holes", open:true, cost:75
+    king_tut = Exhibit.create! name:"King Tut", open:true, cost:40
+    before_us= Exhibit.create! name:"Before Us", open:true, cost:35
+
+    expected = Exhibit.search_by_name("Before Us")
+    expect(expected).to eq(before_us)
+    expect(expected).to_not eq(king_tut)
+  end
 end
